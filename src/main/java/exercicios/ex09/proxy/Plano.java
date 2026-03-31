a5954cef107f8a268065bbbb867666e0fa60fe8c
Stefany Maria <sthefan1morais@gmail.com>
2026-03-31 19:49:08 -0300

    atividade 9

--- src/main/java/exercicios/ex09/proxy/Plano.java
+++ src/main/java/exercicios/ex09/proxy/Plano.java
@@ -2,14 +2,5 @@ package exercicios.ex09.proxy;
 
 public enum Plano {
     BASICO,
-    PREMIUM,;
-
-    public String getPlano() {
-        if (this == BASICO) {
-            return "Básico";
-        } else if (this == PREMIUM) {
-            return "Premium";
-        }
-        return "Plano desconhecido";
-    }
+    PREMIUM;
 }
